59efcd137aab4c6d99abaafb75e5571a7c94a00e
Marvin Reza <marvinreza@outlook.com>
2018-04-24 18:19:15 +0200

    Now draws different colors for different detections

--- ssd_to_pascal.py
+++ ssd_to_pascal.py
@@ -4,24 +4,26 @@ from os.path import isfile, join
 from detector import Detector
 from pascal_writer import PascalWriter
 from PIL import Image, ImageDraw
+import numpy
 
 visualize_detections = True
 
 def draw_detection(xmin, ymin, xmax, ymax, name, conf):
-    draw.rectangle([xmin, ymin, xmax, ymax], outline=(255, 0, 0))
+    color = {"red" : (255,50,50), "green" : (0,255,0), "tower" : (245,245,245)} 
+    draw.rectangle([xmin, ymin, xmax, ymax], outline=color[name])
     draw.text([xmin, ymin], name + ": " + str(conf), (0, 0, 255))
 
 # Configurations for neural network
 gpu_id = 0
-image_resize = 300
+image_resize  = 300
 data_folder   = "data/"
 model_def     = data_folder + "deploy.prototxt"
-model_weights = data_folder + "VGG_Ascend_SSD_Color_300x300_iter_24000.caffemodel"
+model_weights = data_folder + "old.caffemodel"
 labelmap_file = data_folder + "labelmap.prototxt"
 
 # Configurations for input images
-image_dir = "output/images/"
-xml_dir = "output/xml/"
+image_dir = "../data/ascend/new_training_data/"
+xml_dir = "../data/ascend/new_training_data_xml/"
 image_files = [f for f in listdir(image_dir) if isfile(join(image_dir, f))]
 
 detector = Detector(gpu_id, model_def, model_weights, image_resize, labelmap_file)
@@ -35,7 +37,7 @@ for image_file in image_files:
     if visualize_detections:
         draw = ImageDraw.Draw(img)
 
-    writer = PascalWriter(image_dir, os.path.basename(image_file), img.size)
+    writer = PascalWriter(image_dir, os.path.basename(image_file), numpy.array(img).shape)
     for item in result:
         xmin, ymin, xmax, ymax = item[0], item[1], item[2], item[3]
         name, conf = item[-1], item[-2]
